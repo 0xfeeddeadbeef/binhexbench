d619b6052010005dd975915379b2413e204c6c28
George Chakhidze <0xfeeddeadbeef@gmail.com>
2018-06-30 17:31:34 +0400

    Add fastest variant

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime;
+using System.Runtime.InteropServices;
 using System.Text;
 using BenchmarkDotNet;
 using BenchmarkDotNet.Analysers;
@@ -17,7 +19,7 @@ public static class Program
 
 [MemoryDiagnoser]
 // [DisassemblyDiagnoser(true, false, false, false, 1)]
-public class HexBench
+public unsafe class HexBench
 {
     // From small arrays to big enough to go into LOH
     [Params(4, 40, 400, 4000, 8000, 40000, 80000, 120000, 240000)]
@@ -72,6 +74,45 @@ public class HexBench
         return result.ToString();
     }
 
+    // From: https://stackoverflow.com/questions/311165/how-do-you-convert-a-byte-array-to-a-hexadecimal-string-and-vice-versa/24343727#24343727
+    private static readonly uint[] _lookup32Unsafe = CreateLookup32Unsafe();
+    private static readonly uint* _lookup32UnsafeP = (uint*)GCHandle.Alloc(_lookup32Unsafe, GCHandleType.Pinned).AddrOfPinnedObject();
+
+    private static uint[] CreateLookup32Unsafe()
+    {
+        var result = new uint[256];
+
+        for (int i = 0; i < 256; i++)
+        {
+            string s = i.ToString("X2");
+            if (BitConverter.IsLittleEndian)
+                result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+            else
+                result[i] = ((uint)s[1]) + ((uint)s[0] << 16);
+        }
+
+        return result;
+    }
+
+    public static string ByteArrayToHexViaLookup32Unsafe(byte[] bytes)
+    {
+        var lookupP = _lookup32UnsafeP;
+        var result = new char[bytes.Length * 2];
+
+        fixed (byte* bytesP = bytes)
+        fixed (char* resultP = result)
+        {
+            uint* resultP2 = (uint*)resultP;
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                resultP2[i] = lookupP[bytesP[i]];
+            }
+        }
+
+        return new string(result);
+    }
+
     [Benchmark]
     public string BinaryToHex_Benchmark()
     {
@@ -82,5 +123,11 @@ public class HexBench
     public string BytesToString_Benchmark()
     {
         return BytesToString(this.data);
+    }
+
+    [Benchmark]
+    public string ByteArrayToHex_Benchmark()
+    {
+        return ByteArrayToHexViaLookup32Unsafe(this.data);
     }
 }
